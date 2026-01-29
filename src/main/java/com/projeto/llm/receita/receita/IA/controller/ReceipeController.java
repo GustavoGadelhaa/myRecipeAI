815bf75df106ecfdbce691bff4987d5db4249404
Gustavo Gadelha <gustavogads4@gmail.com>
2026-01-28 22:37:22 -0300

    add:logs

--- src/main/java/com/projeto/llm/receita/receita/IA/controller/ReceipeController.java
+++ src/main/java/com/projeto/llm/receita/receita/IA/controller/ReceipeController.java
@@ -5,18 +5,25 @@ import com.projeto.llm.receita.receita.IA.dtos.UserResponseDTO;
 import com.projeto.llm.receita.receita.IA.service.ReceipeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/receipe")
 @RequiredArgsConstructor
+@Slf4j
 public class ReceipeController {
 
     private final ReceipeService receipeService;
 
     @PostMapping
     public ResponseEntity<UserResponseDTO>getReceipe(@Valid @RequestBody UserRequestDTO requestDTO){
+        log.info("{} fez uma request às {}", requestDTO.getEmail(), ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
         UserResponseDTO response = receipeService.returnReceipeIaInEmail(requestDTO);
         return ResponseEntity.ok(response);
 
--- src/main/java/com/projeto/llm/receita/receita/IA/notificationAdm/NotificationAdm.java
+++ src/main/java/com/projeto/llm/receita/receita/IA/notificationAdm/NotificationAdm.java
@@ -2,15 +2,21 @@ package com.projeto.llm.receita.receita.IA.notificationAdm;
 
 import com.projeto.llm.receita.receita.IA.webService.BrevoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @RequiredArgsConstructor
 @Component
-
+@Slf4j
 public class NotificationAdm {
 private final BrevoService brevoService;
     public void sendAlertToAdm(){
+        log.error("Alerta enviado pro ADM as {}", ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
         brevoService.sendEmailToAdm("gustavogads4@gmail.com");
     }
 }
